df716ffc9874c6eca87e4a00f8662143c265d49e
Patrick Johnson <patrick.l.johnson@gmail.com>
2013-03-05 22:29:07 -0800

    added a basic responses

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,18 @@ public class Application extends Controller {
     public static Result index() {
         return ok("Beer Me");
     }
-  
+
+    public static Result fullOutput(String style) {
+        String response = "";
+        if (style.equals("porter")) {
+            response = "{style: \"porter\"}";
+        }
+        else if (style.equals("stout")) {
+            response = "{style: \"stout\"}";
+        }
+        else {
+            response = "{style: \"bud-lite\"}";
+        }
+        return ok(response);
+    }
 }
