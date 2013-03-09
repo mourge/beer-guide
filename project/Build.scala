f1c74bd424f9f064ccabcf16afb428b6b78462e6
Patrick Johnson <patrick.l.johnson@gmail.com>
2013-03-08 16:09:44 -0800

    Adding dependency: json-simple

--- project/Build.scala
+++ project/Build.scala
@@ -11,11 +11,11 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     javaCore,
     javaJdbc,
-    javaEbean
+    javaEbean,
+    "com.googlecode.json-simple" % "json-simple" % "1.1.1"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
   )
-
 }
